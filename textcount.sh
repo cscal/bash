4565efc2881f8c7c17b0271f6991ce228e00cc69
cscal <cscal@protonmail.com>
2019-03-27 17:18:12 -0400

    added comments and fixed bug when there are no text files

--- textcount.sh
+++ textcount.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-i=0
-for file in *.txt
-do
-    ((i++))
-done
-echo 'There are '$i 'txt files.'
+# This script accepts a command line argument for a directory to search
+# for text files, then counts the number of text files.
+
+# If there aren't any text files, don't enter the loop. Corrects i value.
+ls $1/*.txt &> /dev/null
+if [ $? -ne 0 ]
+then
+    echo 'There are 0 files ending in .txt. If you were expecting output,
+        ensure that you typed the directory name correctly as the argument.'
+else
+    i=0
+    for file in $1/*.txt
+    do
+        ((i++))
+    done
+    echo 'There are '$i 'files ending in .txt'
+fi
--- user.sh
+++ user.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-read -p 'Enter username: ' NAME
+# A user-friendly script for creating users with the most commonly used options
+# This asks for the password which is poor for security; it's just for demo
 
+read -p 'Enter username: ' NAME
 read -p 'Enter user id: ' UIDD
 read -p 'Enter the password: ' PASS
-
 read -p 'Primary group: ' PRIME
-
 read -p 'Secondary groups: ' SEC
 
 useradd -u $UIDD -g $PRIME -G $SEC -p `openssl passwd $PASS` $NAME
