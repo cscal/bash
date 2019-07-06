ca11560f06ef7418b83f8f96fd70c8868e3716ff
cscal <cscal@protonmail.com>
2019-07-06 17:02:02 -0400

    Created install script for k8s workers

---
+++ kubeworker.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Ensure swap is disabled
+swapoff -a
+# use sed to find the swap line of /etc/fstab and comment it out
+sed -i '/swap/ s/^/#/' /etc/fstab
+
+# Disable SELinux
+setenforce 0
+sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
+
+# Configure firewall
+firewall-cmd --permanent --add-port=6443/tcp
+firewall-cmd --permanent --add-port=10251/tcp
+firewall-cmd --permanent --add-port=10255/tcp
+firewall-cmd --reload
+
+# Set bridge in conf
+cat > /etc/sysctl.d/k8s.conf << EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+# Configure the kubernetes repo
+cat > /etc/yum.repos.d/kubernetes.repo << EOF
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg  https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+# Make all changes
+sysctl --system
+
+# Install Docker and Kubernetes
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+export VERSION=18.06 && curl -sSL get.docker.com | sh
+yum install -y kubeadm kubelet kubectl
+systemctl start docker && systemctl enable docker
+systemctl start kubelet && systemctl enable kubelet
+
+# end of script
+echo 'Script finished. If all went well, copy contents of master token to each worker, which joins the workers to the master'
+echo 'Then run "kubectl get nodes"'
