fc65b3ca5f2ed829141c6f8f330ba6499bcbdfdc
Charlie Calhoun <cscal@protonmail.com>
2019-08-27 14:55:00 -0400

    pseudocode about automating static ip setup

--- network-setup.sh
+++ network-setup.sh
@@ -3,15 +3,23 @@
 # This script automatically configures networking.
 # A future feature is doing this for all servers on the subnet.
 
-#cp /etc/sysconfig/network-scripts/ifcfg-enp0s3 /etc/sysconfig/network-scripts/ifcfg-enp0s3.bak
+# I think the best way to automate network config for large
+# numbers of non-cloud servers is to create a PXE kickstart file.
+# However kickstart only understands variables in Pre- and Post-
+# installation scripts, or in scripts run in Post- mounted on NFS.
+# Every time the script runs it needs to increment an IP variable
+# and save that variable to a persistent text file.
+# Loops won't work because the script is run anew on each system.
+
+# So the below is just an example of using sed to set static IP
 
 sed -i[backup] {
 s/BOOTPROTO=dhcp/BOOTPROTO=static
 s/ONBOOT=no/ONBOOT=yes
 s/NM_CONTROLLED=yes/NM_CONTROLLED=no
-IPADDR=192.168.
+IPADDR=192.168.1.3
 NETMASK=255.255.255.0
 GATEWAY=192.168.1.1
-DNS=
+DNS=8.8.8.8
 n
 } /etc/sysconfig/network-scripts/ifcfg-enp0s3
